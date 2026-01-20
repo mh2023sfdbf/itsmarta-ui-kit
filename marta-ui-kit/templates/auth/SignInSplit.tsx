ff994a40e3039a150a6060401b0a7a2b5d9bb99b
mh2023sfdbf <marta@wohnly.com>
2026-01-20 15:25:31 +0000

    Fix hydration error in SignInSplit by adding suppressHydrationWarning to parent container

--- marta-ui-kit/templates/auth/SignInSplit.tsx
+++ marta-ui-kit/templates/auth/SignInSplit.tsx
@@ -9,7 +9,7 @@ export default function SignInSplit() {
       className="relative min-h-screen py-16 md:py-32 bg-[#f7f6f4]" 
       style={{ backgroundImage: "url('flex-ui-assets/elements/pattern-white.svg')", backgroundPosition: 'center' }}
     >
-      <div className="container px-4 mx-auto pb-16">
+      <div className="container px-4 mx-auto pb-16" suppressHydrationWarning>
         <div className="w-full md:w-3/5 lg:w-1/2">
           <div className="max-w-sm mx-auto">
             <div className="mb-6 text-center">
